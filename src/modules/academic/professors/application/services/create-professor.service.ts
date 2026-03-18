201ea8253c9dad693d9e143e8cdb60ca30d67041
Victor Blum Nisczak <blumnisczak@gmail.com>
2026-03-18 19:51:23 -0300

    feat(professors): refactor create service and add list service

---
+++ src/modules/academic/professors/application/services/create-professor.service.ts
@@ -0,0 +1,25 @@
+import { ConflictException, Inject, Injectable } from "@nestjs/common";
+
+import { ProfessorDto } from "@academic/professors/application/dto/professor.dto";
+import { Professor } from "@academic/professors/domain/models/professor.entity";
+import {
+  PROFESSOR_REPOSITORY,
+  type ProfessorRepository,
+} from "@academic/professors/domain/repositories/professor-repository.interface";
+
+@Injectable()
+export class CreateProfessorService {
+  constructor(
+    @Inject(PROFESSOR_REPOSITORY)
+    private readonly professorRepository: ProfessorRepository,
+  ) {}
+
+  async execute(dto: ProfessorDto): Promise<void> {
+    const existing = await this.professorRepository.findByEmail(dto.email);
+    if (existing) {
+      throw new ConflictException("Email already registered");
+    }
+    const professor = Professor.restore(dto);
+    await this.professorRepository.create(professor!);
+  }
+}
---
+++ src/modules/academic/professors/application/services/list-professors.service.ts
@@ -0,0 +1,20 @@
+import { Inject, Injectable } from "@nestjs/common";
+
+import { ProfessorDto } from "@academic/professors/application/dto/professor.dto";
+import {
+  PROFESSOR_REPOSITORY,
+  type ProfessorRepository,
+} from "@academic/professors/domain/repositories/professor-repository.interface";
+
+@Injectable()
+export class ListProfessorsService {
+  constructor(
+    @Inject(PROFESSOR_REPOSITORY)
+    private readonly professorRepository: ProfessorRepository,
+  ) {}
+
+  async execute(): Promise<ProfessorDto[]> {
+    const response = await this.professorRepository.findAll();
+    return response.map((row) => ProfessorDto.fromProfessor(row)!);
+  }
+}
